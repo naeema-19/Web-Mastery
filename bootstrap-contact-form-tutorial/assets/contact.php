37356ed2010a723d1e0c3fa9530d1ffdc69cd1d8
naeema-19 <75922224+naeema-19@users.noreply.github.com>
2021-08-29 12:54:29 +0530

    Update contact.php

--- bootstrap-contact-form-tutorial/assets/contact.php
+++ bootstrap-contact-form-tutorial/assets/contact.php
@@ -8,7 +8,7 @@ function isEmail($email) {
 if($_POST) {
 
     // Enter the email where you want to receive the message
-    $emailTo = 'naeemaziyad9@gmail.com';
+    $emailTo = 'naeema_ziyad@ieee.org';
 
     $clientEmail = addslashes(trim($_POST['email']));
     $subject = addslashes(trim($_POST['subject']));
